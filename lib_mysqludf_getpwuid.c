e6df51b16a07830f47d00920a87edea531ecb814
Michael Gutteridge <michael.gutteridge@gmail.com>
2015-09-03 10:27:44 -0700

    first working version

--- lib_mysqludf_getpwuid.c
+++ lib_mysqludf_getpwuid.c
@@ -30,11 +30,11 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-	DLLEXP my_bool getpwuid_init(
+	DLLEXP my_bool lib_mysqludf_getpwuid_init(
             UDF_INIT *initid, UDF_ARGS *args, char *message
             );
-	DLLEXP void getpwuid_deinit(UDF_INIT *initid);
-	DLLEXP char *getpwuid(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error);
+	DLLEXP void lib_mysqludf_getpwuid_deinit(UDF_INIT *initid);
+	DLLEXP char *lib_mysqludf_getpwuid(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error);
 
 #ifdef	__cplusplus
 }
@@ -43,22 +43,35 @@ extern "C" {
 
 #include <pwd.h>
 
-my_bool getpwuid_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
+my_bool lib_mysqludf_getpwuid_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
 {
+    if ( args->arg_count != 1 )
+    {
+        strcpy(message, "Incorrect number of arguments to getpwuid");
+        return 1;
+    }
+    if ( args->arg_type[0] != INT_RESULT )
+    {
+        strcpy(message, "Argument must be integer");
+        return 1;
+    }
 	return 0;
 }
 
-void getpwuid_deinit(UDF_INIT *initid)
+void lib_mysqludf_getpwuid_deinit(UDF_INIT *initid)
 {
 }
 
 /* For functions that return STRING or DECIMAL */ 
-char* getpwuid(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long* length,	char *is_null, char *error)
+char* lib_mysqludf_getpwuid(UDF_INIT *initid, UDF_ARGS *args, char* result, unsigned long* length,	char *is_null, char *error)
 {
     struct passwd *pwd;
-    pwd = getpwuid( args->args[0] );
+    long long uid;
+
+    uid = *((long long*) args->args[0] );
+    pwd = getpwuid( uid );
 	strcpy(result, pwd->pw_name);
-	*length = strlen(pwd->pw_name);
+	*length = strlen(result);
 	return result;
 }
 
