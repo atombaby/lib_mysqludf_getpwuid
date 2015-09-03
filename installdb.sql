24209694458b1f1d3cad8e16f7c4798673c56180
Michael Gutteridge <michael.gutteridge@gmail.com>
2015-09-03 10:28:03 -0700

    updated for function name

--- installdb.sql
+++ installdb.sql
@@ -1,4 +1,4 @@
 USE mysql;
 
 DROP FUNCTION IF EXISTS lib_mysqludf_getpwuid;
-CREATE FUNCTION lib_mysqludf_getpwuid RETURNS STRING SONAME 'lib_mysqludf_skeleton.so';
+CREATE FUNCTION lib_mysqludf_getpwuid RETURNS STRING SONAME 'lib_mysqludf_getpwuid.so';
